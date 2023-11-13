44cac6b7b30eb44ee5e25c04282e41bd7f1756e4
LukasLapienis <lapienislukasp@gmail.com>
2023-11-13 09:46:47 +0200

    new

--- 09List/05list(iteracija).py
+++ 09List/05list(iteracija).py
@@ -1,15 +1,16 @@
 m = [5, 6, 8, 7, 14, 15, -85, -8, 13, 17]
 
 suma = 0
-# for i in m:
-#     suma += i
-#     i = 1000
-# print(suma)
-# print(m)
-
-for i in range(len(m)):
-    suma += m[i]
-    m[i] = 10000
-
+for i in m:
+    suma += i
+    i = 1000
 print(suma)
 print(m)
+
+# for i in range(len(m)):
+#     suma += m[i]
+#     m[i] = 10000
+
+# print(suma)
+# print(m)
+print(len(m))
