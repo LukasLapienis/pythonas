de3a6ac7dc47dc119d943155f83e24ad1267cf1a
LukasLapienis <lapienislukasp@gmail.com>
2023-12-17 19:48:05 +0200

    baigta

--- ciklaiAtsiskaitymas/ciklaiAtsiskaitymas.py
+++ ciklaiAtsiskaitymas/ciklaiAtsiskaitymas.py
@@ -2,27 +2,27 @@
 
 while True:
     try:
-        kaina = float(input('iveskite kavos kaina eurais: '))
-        if kaina <= 0:
+        price = float(input('iveskite kavos kaina eurais: '))
+        if price <= 0:
             print("prasau iveskite teigiama skaiciu")
         else:
             break
     except:
         print("blogai ivesta kaina, bandykite dar karta")
 
-suma = 0
+sum = 0
 countBad = 0
 countGood = 0
-while suma < kaina:
-    liko = kaina - suma
-    print(f'liko sumoketi: {round(liko, 2)} euro')
+while sum < price:
+    leftToPay = price - sum
+    print(f'liko sumoketi: {round(leftToPay, 2)} euro')
     try:    
         Moneta = int(input('Imeskite moneta: '))
         if Moneta == 1 or Moneta == 2:
-            suma = suma + Moneta
+            sum = sum + Moneta
             countGood = countGood + 1
         elif Moneta == 10 or Moneta == 20 or Moneta == 50:
-            suma = suma + Moneta * 0.01
+            sum = sum + Moneta * 0.01
             countGood = countGood + 1
         else:           
             print("netinkama moneta, meskite dar karta")
@@ -30,8 +30,8 @@ while suma < kaina:
     except:
         print("Nezinau ka idejote, bet ne moneta, bandykite dar karta")
 else:
-    liko = kaina - suma
-    print(f'jusu graza {round(liko * -1, 2)} euro, skanios kavos!')
+    leftToPay = price - sum
+    print(f'jusu graza {round(leftToPay * -1, 2)} euro, skanios kavos!')
     print(f'imesta tikru monetu: {countGood}, padirbtu: {countBad}')
 
 
@@ -40,18 +40,17 @@ else:
 
 while True:
     try:
-        number = int(input('iveskite skaiciu ir pavaizduosiu zvaigzdutemis: '))
+        inputNumber = int(input("Input a number: "))
         break
     except:
-        print("blogai ivestas skaicius, bandykite dar karta")
+        print("wrong input, try again.")
 
-numberStr = str(number)
-length = len(numberStr)
-print(f'skaicius sudarytas is {length} skaitmenu')
+numberStr = str(inputNumber)
+inputNumberLength = len(numberStr)
 
 i = 0
-while i < length:
-    print("*" * int(numberStr[length-i-1]))
+while i < inputNumberLength:
+    print("*" * int(numberStr[inputNumberLength-i-1]))
     i += 1
 
 # #-------------------------------------------------------------------------------------------
