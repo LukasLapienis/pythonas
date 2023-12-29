b2525ad52adf6ba6297dfc493406e9d731729865
LukasLapienis <lapienislukasp@gmail.com>
2023-12-29 22:34:59 +0200

    1baigta

--- failai2Atsiskaitymas/failai2Atsiskaitymas.py
+++ failai2Atsiskaitymas/failai2Atsiskaitymas.py
@@ -1,11 +1,11 @@
 # -----------------------------------Skaiciu kovos---------------------------------------------------------------
 
 def clear():
-    with open('failai2Atsiskaitymas/skaiciuKovos.txt', 'w') as f:
+    with open('failai2Atsiskaitymas/rez.txt', 'w') as f:
         pass
 
 def record(txt):
-    with open('failai2Atsiskaitymas/skaiciuKovos.txt', 'a', encoding='utf-8') as file:
+    with open('failai2Atsiskaitymas/rez.txt', 'a', encoding='utf-8') as file:
         file.write(f'{txt}\n')
 
 def readAndList():
@@ -16,19 +16,26 @@ def readAndList():
     return splited
 
 startingNumbers = readAndList()
-print(startingNumbers)
 
-
-while len(startingNumbers) > 1:
-    if startingNumbers[0] == startingNumbers[1]:
-        startingNumbers[0] = startingNumbers[0] + startingNumbers[1]
-        startingNumbers.pop(1)
-    elif startingNumbers[0] > startingNumbers[1]:
-        startingNumbers[0] = startingNumbers[0] + startingNumbers[0] - startingNumbers[1]
-        startingNumbers.pop(1)
-    elif startingNumbers[0] < startingNumbers[1]:
-        startingNumbers[0] = startingNumbers[1] + startingNumbers[1] - startingNumbers[0]
-        startingNumbers.pop(1)
-    
-    print(startingNumbers)
-
+def fight():
+    clear()
+    round = 0
+    while len(startingNumbers) > 1:
+        round += 1
+        if startingNumbers[0] == startingNumbers[1]:
+            startingNumbers[0] = startingNumbers[0] + startingNumbers[1]
+            startingNumbers.pop(1)
+            record(f'{round} kova. Lygiosios. Skaiciai susijungia.')
+        elif startingNumbers[0] > startingNumbers[1]:
+            startingNumbers[0] = startingNumbers[0] + startingNumbers[0] - startingNumbers[1]
+            startingNumbers.pop(1)
+            record(f'{round} kova. Pirmas skaicius sunaikina antra skaiciu.')
+        elif startingNumbers[0] < startingNumbers[1]:
+            startingNumbers[0] = startingNumbers[1] + startingNumbers[1] - startingNumbers[0]
+            startingNumbers.pop(1)
+            record(f'{round} kova. Antras skaicius sunaikina pirma skaiciu.')
+        
+        extractedList = ' '.join(str(x) for x in startingNumbers)
+        record(f'Rezultatas {extractedList}')
+        
+fight()
