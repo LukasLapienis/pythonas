48f5f503e179daed9702e01b33d0acfe4e75f0d9
LukasLapienis <lapienislukasp@gmail.com>
2023-12-30 22:44:14 +0200

    2

--- failai2Atsiskaitymas/skaiciuKovos.py
+++ failai2Atsiskaitymas/skaiciuKovos.py
@@ -1,11 +1,11 @@
 # -----------------------------------Skaiciu kovos---------------------------------------------------------------
 
 def clear():
-    with open('failai2Atsiskaitymas/rez.txt', 'w') as f:
+    with open('failai2Atsiskaitymas/skaiciuKovosRez.txt', 'w') as f:
         pass
 
 def record(txt):
-    with open('failai2Atsiskaitymas/rez.txt', 'a', encoding='utf-8') as file:
+    with open('failai2Atsiskaitymas/skaiciuKovosRez.txt', 'a', encoding='utf-8') as file:
         file.write(f'{txt}\n')
 
 def readAndList():
@@ -24,18 +24,20 @@ def fight():
         round += 1
         if startingNumbers[0] == startingNumbers[1]:
             startingNumbers[0] = startingNumbers[0] + startingNumbers[1]
-            startingNumbers.pop(1)
             record(f'{round} kova. Lygiosios. Skaiciai susijungia.')
         elif startingNumbers[0] > startingNumbers[1]:
             startingNumbers[0] = startingNumbers[0] + startingNumbers[0] - startingNumbers[1]
-            startingNumbers.pop(1)
             record(f'{round} kova. Pirmas skaicius sunaikina antra skaiciu.')
         elif startingNumbers[0] < startingNumbers[1]:
             startingNumbers[0] = startingNumbers[1] + startingNumbers[1] - startingNumbers[0]
-            startingNumbers.pop(1)
             record(f'{round} kova. Antras skaicius sunaikina pirma skaiciu.')
-        
+            
+        startingNumbers.pop(1)
         extractedList = ' '.join(str(x) for x in startingNumbers)
         record(f'Rezultatas {extractedList}')
         
 fight()
+
+
+
+
