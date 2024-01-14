12ed696ee0c0631d1391c209931a1b7eb8d6ec41
LukasLapienis <lapienislukasp@gmail.com>
2024-01-14 22:31:47 +0200

    baigta

--- dvimatisMasyvas.py
+++ dvimatisMasyvas.py
@@ -11,19 +11,23 @@ def gautiSarasa():
         duMas.append(eil)
     return duMas
     
-def rezultatas():
-    duList = gautiSarasa()
-    print(duList)
-    with open('dvimatisMasyvasRez.txt', 'w') as f:
-        for row in duList:
-            t = str(row)
-            txt = t[1:-1].replace(', ', '\t')
-            f.write(f'{txt}\n')
+# def rezultatas():
+#     duList = gautiSarasa()
+#     print(duList)
+#     with open('dvimatisMasyvasRez.txt', 'w') as f:
+#         for row in duList:
+#             t = str(row)
+#             txt = t[1:-1].replace(', ', '\t')
+#             f.write(f'{txt}\n')
             
-rezultatas()
+# rezultatas()
 
-sk = random.randint(-100, 100)
-print(sk)
-
-def skaiciavimai():
-    pass
+# sk = random.randint(-100, 100)
+# print(sk)
+matrix = gautiSarasa()
+averages = []
+for row in matrix: 
+ print(row)
+ print(sum(row)/len(row))
+ averages.append(sum(row)/len(row))
+print(averages)
